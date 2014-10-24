6321f9a885b66e3a3285e31c6130be8b3798594d
Bryan <bkesteloo@outlook.com>
2014-10-24 09:01:54 -0700

    Assignment 3 Start

---
+++ Assignments/Assignment 3/src/LLStack.java
@@ -0,0 +1,25 @@
+
+public class LLStack<T> implements Stack<T> 
+{
+	
+	public int size()
+	{
+		return count;
+	}
+	
+	public boolean empty()
+	{
+		if (count == 0)
+		{
+			return true;
+		}
+		
+		else
+		{
+			return false;
+		}
+	}
+	
+	
+
+}
